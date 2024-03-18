3b3b294b871dc6ff10b3432ca3c4a17e165446ca
Nicolaj <nicolaj215@gmail.com>
2024-03-18 21:45:19 +0100

    view cart and delete items

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const Cart = require("./cart");
+
 const p = path.join(
   path.dirname(require.main.filename),
   "data",
@@ -10,7 +12,14 @@ const p = path.join(
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      cb([]);
+      if (err.code === 'ENOENT') {
+        // File does not exist, return empty array
+        cb([]);
+      } else {
+        // Other error occurred, handle it appropriately
+        console.error('Error reading file:', err);
+        cb([]);
+      }
     } else {
       cb(JSON.parse(fileContent));
     }
@@ -45,6 +54,23 @@ module.exports = class Product {
       }
     });
   }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => {
+        return prod.id === id;
+      });
+      const updatedProducts = products.filter((prod) => {
+        return prod.id !== id;
+      });
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
